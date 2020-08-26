09fb48b6343afaffa65acbedb1769698bb2ae9b9
Manjunath Reddy <manju.reddys@gmail.com>
2020-08-25 23:53:16 -0700

    few more changes

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ To start the run `npx ng serve api`
 
 After application starts goto `http://localhost:3333/api/v1/hello` => Should print in the terminal "Gateway checked".
 
-When run the GraphQl query that message won't show
+When run the GraphQl query that message won't show `http://localhost:3333/api/v1/graphql`
--- apps/api/src/app/gateway/gateway.middleware.ts
+++ apps/api/src/app/gateway/gateway.middleware.ts
@@ -3,7 +3,7 @@ import { Injectable, NestMiddleware } from "@nestjs/common";
 @Injectable()
 export class GatewayMiddleware implements NestMiddleware {
     use(req: any, res: any, next: () => void) {
-        console.log('Gateway checked');
+        console.log('\n\nGateway checked\n\n');
         next();
     }
 }
